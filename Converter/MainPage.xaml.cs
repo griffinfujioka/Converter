8fd35890741c70dacc07a8696f95ce554119ef27
Griffin Fujioka <fujiokag@hotmail.com>
2012-09-11 04:42:21 -0700

    second commit
    Implemented real-time conversions and input validation. I don't have
    total faith in my input validation but it gets the job done.  TODO:
    - Icons and splash screen
    - Use toggle to hide/view keyboard. Correlate whatever textbox has focus
    with the buttons enabled on keyboard (I.e., 1 and 0 for binary, 0-9A-F
    for hex and 0-9 for decimal

--- Converter/MainPage.xaml.cs
+++ Converter/MainPage.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups; 
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -21,13 +22,19 @@ namespace Converter
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        public MainPage()
+        #region Some variable declarations
+        string focusedTxtBox = "";
+        string binaryLine;
+        string decLine; 
+        string hexLine;
+        #endregion
+
+        public  MainPage()
         {
             this.InitializeComponent();
-
-            
         }
 
+        #region On Navigated To
         /// <summary>
         /// Invoked when this page is about to be displayed in a Frame.
         /// </summary>
@@ -36,112 +43,169 @@ namespace Converter
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
         }
+        #endregion
 
-        public void textChanged(string sender, string line)
+        #region textChanged()
+        public void textChanged()
         {
             int decValue;
             string hexValue;
             string binaryValue;
-            switch (sender)
+            switch (focusedTxtBox)
             {
                 case "binary":
-                    decValue = Convert.ToInt32(line);
-                    hexValue = decValue.ToString("X");
-                    hexTxtBox.Text = hexValue;
-                    decimalTxtBox.Text = decValue.ToString();
+                    if (binaryLine != "")
+                    {
+                        decValue = Convert.ToInt32(binaryLine, 2);
+                        hexValue = decValue.ToString("X");
+                        hexTxtBox.Text = hexValue;
+                        decimalTxtBox.Text = decValue.ToString();
+                    }
+                    else
+                    {
+                        hexTxtBox.Text = "";
+                        decimalTxtBox.Text = "";
+                    }
                     break; 
                 case "decimal":
-                    decValue = Convert.ToInt32(line);
-                    binaryValue = Convert.ToString(decValue, 2);
-                    hexValue = decValue.ToString("X");
-                    hexTxtBox.Text = hexValue;
-                    binaryTxtBox.Text = binaryValue;
+                    if (decLine != "")
+                    {
+                        decValue = Convert.ToInt32(decLine);
+                        binaryValue = Convert.ToString(decValue, 2);
+                        hexValue = decValue.ToString("X");
+                        hexTxtBox.Text = hexValue;
+                        binaryTxtBox.Text = binaryValue;
+                    }
+                    else
+                    {
+                        hexTxtBox.Text = "";
+                        binaryTxtBox.Text = "";
+                    }
                     break; 
                 case "hex":
-                    decValue = int.Parse(line, System.Globalization.NumberStyles.HexNumber);
-                    binaryValue = Convert.ToString(decValue, 2);
-                    binaryTxtBox.Text = binaryValue;
-                    decimalTxtBox.Text = decValue.ToString();
+                    if (hexLine != "")
+                    {
+                        decValue = int.Parse(hexLine, System.Globalization.NumberStyles.HexNumber);
+                        binaryValue = Convert.ToString(decValue, 2);
+                        binaryTxtBox.Text = binaryValue;
+                        decimalTxtBox.Text = decValue.ToString();
+                    }
+                    else
+                    {
+                        binaryTxtBox.Text = "";
+                        decimalTxtBox.Text = "";
+                    }
                     break; 
                 default: break; 
             }
         }
+        #endregion
 
+        #region hexTxtBox_TextChanged
         private void hexTxtBox_TextChanged_1(object sender, TextChangedEventArgs e)
         {
-            string line;
-            string hexValue = ""; 
-            string binaryValue;
-            int decValue;
-
-            hexTxtBox.Text = sender.ToString(); 
-
-            line = hexTxtBox.Text;
-
-            textChanged("hex", line); 
-            //if (line != "")
-            //{
-            //    decValue = int.Parse(line, System.Globalization.NumberStyles.HexNumber);
-            //    binaryValue = Convert.ToString(decValue, 2);
-            //    binaryTxtBox.Text = binaryValue;
-            //    decimalTxtBox.Text = decValue.ToString();
-            //}
-            //else
-            //{
-            //    binaryTxtBox.Text = "";
-            //    decimalTxtBox.Text = ""; 
-            //}
+            hexLine = hexTxtBox.Text;
+            if(OnlyHexValues(hexLine) || hexLine == "")
+                textChanged(); 
+            else
+            {
+                hexLine = hexLine.Remove(hexLine.Length -1 , 1);  // remove the last value
+                hexTxtBox.Text = hexLine;
+                hexTxtBox.Select(hexLine.Length, 0); 
+            }
+                
         }
+        #endregion
 
+        #region decimalTxtBox_TextChanged
         private void decimalTxtBox_TextChanged_1(object sender, TextChangedEventArgs e)
         {
-            string line;
-            int decValue;
-            string binaryValue;
-            string hexValue; 
-            
-            line = decimalTxtBox.Text;
-            textChanged("decimal", line); 
-            //if (line != "")
-            //{
-            //    decValue = Convert.ToInt32(line);
-            //    binaryValue = Convert.ToString(decValue, 2);
-            //    hexValue = decValue.ToString("X");
-            //    hexTxtBox.Text = hexValue;
-            //    binaryTxtBox.Text = binaryValue;
-            //}
-            //else
-            //{
-            //    hexTxtBox.Text = "";
-            //    binaryTxtBox.Text = ""; 
-            //}
-            
-        }
+            decLine = decimalTxtBox.Text;
+            if (OnlyIntValues(decLine) || decLine == "")
+                textChanged();
+            else
+            {
+                decLine = decLine.Remove(decLine.Length - 1, 1);
+                decimalTxtBox.Text = decLine;
+                if (decLine.Length == 0)
+                    decimalTxtBox.Select(0, 0); 
+                else
+                    decimalTxtBox.Select(binaryLine.Length, 0); 
+            }
 
+        }
+        #endregion
+
+        #region binaryTxtBox_TextChanged
         private void binaryTxtBox_TextChanged_1(object sender, TextChangedEventArgs e)
         {
-            string line = string.Empty; 
-            string hexValue;
-            int decValue;
-            
-            
-            line = binaryTxtBox.Text;
-            textChanged("binary", line); 
-            //if (line != "")
-            //{
-            //    decValue = Convert.ToInt32(line);
-            //    hexValue = decValue.ToString("X");
-            //    hexTxtBox.Text = hexValue;
-            //    decimalTxtBox.Text = decValue.ToString();
+            binaryLine = binaryTxtBox.Text;
+            if (OnlyBinaryValues(binaryLine) || binaryLine == "")
+                textChanged();
+            else
+            {
+                binaryLine = binaryLine.Remove(binaryLine.Length - 1, 1);
+                binaryTxtBox.Text = binaryLine;
+                binaryTxtBox.Select(binaryLine.Length, 0); 
+            }
+        }
+        #endregion
 
-            //}
-            //else
-            //{
-            //    hexTxtBox.Text = "";
-            //    decimalTxtBox.Text = ""; 
-            //}
+        #region txtBox GotFocus events - change value of focusedTxtBox
+        private void binaryTxtBox_GotFocus_1(object sender, RoutedEventArgs e)
+        {
+            focusedTxtBox = "binary";
+            //textChanged(); 
         }
 
-        
+        private void decimalTxtBox_GotFocus_1(object sender, RoutedEventArgs e)
+        {
+            focusedTxtBox = "decimal";
+            //textChanged(); 
+        }
+
+        private void hexTxtBox_GotFocus_1(object sender, RoutedEventArgs e)
+        {
+            focusedTxtBox = "hex"; 
+            //textChanged(); 
+        }
+        #endregion
+
+        #region Clear button click
+        private void clearButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            binaryLine = "";
+            binaryTxtBox.Text = "";
+
+            decLine = "";
+            decimalTxtBox.Text = "";
+
+            hexLine = "";
+            hexTxtBox.Text = ""; 
+        }
+        #endregion 
+
+        #region Input validation functions
+        public bool OnlyHexValues(string line)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(line, @"\A\b[0-9a-fA-F]+\b\Z"); 
+        }
+ 
+
+        public bool OnlyBinaryValues(string line)
+        {
+            foreach (var c in line)
+                if (c != '0' && c != '1')
+                    return false;
+
+            return true; 
+        }
+
+        public bool OnlyIntValues(string line)
+        {
+            int number;
+            return int.TryParse(line, out number); 
+        }
+        #endregion        
     }
 }
